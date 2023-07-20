b315f71f2006e66afafa06a63fe84a0685ad9568
Dina-petrick <dinapetrick@gmail.com>
2023-07-20 19:35:53 +0530

    feat: refactor the code

--- src/js/domManipulation.js
+++ src/js/domManipulation.js
@@ -1,24 +1,23 @@
+const profileData = JSON.parse(localStorage.getItem("uploadProfile")) || { name: "Daniel Haribo" , image: "https://images.unsplash.com/photo-1687360440984-3a0d7cfde903?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw0Nnx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60"};
 const artistCardList = document.querySelector('.artist-card-list');
 const playListCardList = document.querySelector('.playlist-card-list');
 const recentPlayList = document.querySelector('.recent-playlist-list');
 const artistList = document.querySelector('.artist-list');
+const profileModal = document.getElementById("profile-modal");
+const displayUsername = document.getElementById("artist-card-greet");
 const profileNameElement = document.getElementById("user-profile-name");
 const profilePictureElement = document.getElementById("user-profile-picture");
-export const profilePicture = document.getElementById("upload-profile-pic");
+const profileNameInput = document.getElementById("upload-profile-name");
+const previewProfilePic = document.getElementById("upload-profile-pic");
+const favoriteList = document.getElementById('favorite-list');
 
-const url = "https://raw.githubusercontent.com/Dina-petrick/musicData/main/db.json";
 
 const getData = async () => {
+    const url = "https://raw.githubusercontent.com/Dina-petrick/musicData/main/db.json";
     const response = await fetch(url);
     const data = await response.json();
-    artistCard(data.musicData);
-    createPlaylistCard(data.playlistData);
-    createRecentPlaylistItem(data.musicData);
-    createArtist(data.musicData);
+    localStorage.setItem('fetchedData', JSON.stringify(data));
 }
-
-getData();
-
 
 const createArtistCard = (data, index) => {
     const artistCardItem = document.createElement("li");
@@ -41,9 +40,9 @@ const createArtistCard = (data, index) => {
 }
 
 const artistCard = (dataList) => {
-    dataList.forEach((data,index) => {
-    const artistCardItem =  createArtistCard(data, index);
-    artistCardList.appendChild(artistCardItem);
+    dataList.forEach((data, index) => {
+        const artistCardItem = createArtistCard(data, index);
+        artistCardList.appendChild(artistCardItem);
     })
 }
 
@@ -67,9 +66,10 @@ const createPlaylistCard = (dataList) => {
     })
 }
 
-
 const createRecentPlaylistItem = (dataList) => {
-    dataList.forEach((data) => {
+
+    let shufflePlaylist = shuffleData(dataList) 
+    shufflePlaylist.forEach((data) => {
         const recentPlaylistItem = document.createElement("li");
         recentPlaylistItem.className = "recent-playlist-item";
         recentPlaylistItem.innerHTML = `
@@ -102,15 +102,85 @@ const createArtist = dataList => {
     })
 }
 
-export const displayUserProfile = (data) => {
-        profileNameElement.textContent = data.name;
-        profilePictureElement.src = data.image;
-        profilePicture.src = data.image;
+const favoriteCard = (dataList) => {
+    dataList.forEach((data, index) => {
+        const artistCardItem = createArtistCard(data, index);
+        favoriteList.appendChild(artistCardItem);
+    })
 }
 
 
+const shuffleData = (data) => {
+    const shuffledArr = data.slice();
+    for(let i= shuffledArr.length - 1; i > 0; i--){
+        const randomIndex = Math.floor(Math.random() * (i + 1));
+        [shuffledArr[i], shuffledArr[randomIndex]] = [shuffledArr[randomIndex], shuffledArr[i]]
+    }
+    return shuffledArr;
+}
 
 
+export const getDataUrl = (e) => {
+    let file = e.target.files[0];
+    let fileReader = new FileReader();
+    fileReader.onload = (e) => {
+        previewProfilePic.src = e.target.result;
+    }
+    fileReader.readAsDataURL(file);
+    console.log("saveProfile function called");
+
+}
+
+export const displayUserProfile = (data) => {
+    profileNameElement.textContent = data.name;
+    displayUsername.textContent = `Hello ${data.name}`;
+    if (data.image) {
+        profilePictureElement.src = data.image;
+    }
+}
+export const saveProfile = () =>{
+    if(profileNameInput.value !== "" ){
+        profileData.name = profileNameInput.value;
+    }
+    profileData.image = previewProfilePic.src;
+    localStorage.setItem('uploadProfile', JSON.stringify(profileData));
+    profileNameInput.value = "";
+    displayUserProfile(profileData);
+
+}
+
+export const PreviewProfile = () => {
+    profileModal.style.display = "block";
+    previewProfilePic.src = profileData.image;
+}
+
+export const cancelPreviewProfile = () => {
+    profileModal.style.display = "none";
+    profileNameInput.value = "";
+}
+
+export const displayFetchedData =(data)=> {
+    artistCard(data.musicData);
+    createPlaylistCard(data.playlistData);
+    createRecentPlaylistItem(data.musicData);
+    createArtist(data.musicData);
+}
+
+
+export const toggleList = (show, hidden) => {
+    show.classList.add("hidden");
+    hidden.classList.remove("hidden");
+}
+
+export const hideList = (library, favorite) =>{
+    library.classList.add("hidden");
+    favorite.classList.add("hidden");
+}
+
+await getData();
+const dataFetched = JSON.parse(localStorage.getItem("fetchedData"));
+displayFetchedData(dataFetched);
+displayUserProfile(profileData);
 
 
 
