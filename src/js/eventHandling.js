b7214140cc678761a4d7f52effa7e9cea804c37e
Dina-petrick <dinapetrick@gmail.com>
2023-07-20 19:32:02 +0530

    feat: refactor code

--- src/js/eventHandling.js
+++ src/js/eventHandling.js
@@ -1,48 +1,46 @@
-// local storage 
-export const profileData = JSON.parse(localStorage.getItem("uploadProfile")) || { name: "Daniel Haribo" , image: "https://images.unsplash.com/photo-1687360440984-3a0d7cfde903?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw0Nnx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60"};
-
 //import
-import { displayUserProfile} from "./domManipulation";
+import {getDataUrl, saveProfile, PreviewProfile, cancelPreviewProfile, toggleList, hideList} from "./domManipulation";
 
-const profileModal = document.getElementById("profile-modal");
-const profileNameInput = document.getElementById("upload-profile-name");
-const ProfilePicInput = document.getElementById("upload-profile-pic-input");
-export const profilePicture = document.getElementById("upload-profile-pic");
+const sidebar = document.getElementById("sidebar");
+const profilePicInput = document.getElementById("upload-profile-pic-input");
 
+const playlist = document.getElementById("homepage-container");
+const favoriteList = document.getElementById("favorite-container");
 
-document.body.addEventListener("click", (e) => {
-    let targetElement = e.target;
-    if (targetElement.closest(".user-profile-detail")) {
-        profileModal.style.display = "block";
-    }
-    if (targetElement.closest("#save-profile-btn")) {
-        if(profileNameInput.value !== "" ){
-            profileData.name = profileNameInput.value;
-        }
-        profileData.image = profilePicture.src;
-        localStorage.setItem('uploadProfile', JSON.stringify(profileData));
-        displayUserProfile(profileData);
-        profileNameInput.value = "";
+sidebar.addEventListener("click", (e) => {
+    const targetElement = e.target.id;
+    switch (targetElement) {
+        case "profile-update-btn":
+            PreviewProfile();
+            break;
 
-    }
-    if (targetElement.closest("#cancel-profile-btn")) {
-        profileModal.style.display = "none";
-        profileNameInput.value = "";
+        case "save-profile-btn":
+            saveProfile();
+            break;
+
+        case "cancel-profile-btn":
+            cancelPreviewProfile();
+            break;
+
+        case "library-list":
+            toggleList(favoriteList, playlist);
+            break;
+
+        case "favorites-list":
+            toggleList(playlist, favoriteList);
+            break;
+
+        case "playlist-list":
+        case "artist-list":
+        case "album-list":
+            hideList(playlist,favoriteList);
+            break;
     }
 })
 
-ProfilePicInput.addEventListener("change", (e) => {
-    let file = e.target.files[0];
-    let fileReader = new FileReader();
-    fileReader.onload = (e) => {
-        profilePicture.src = e.target.result;
-    }
-    fileReader.readAsDataURL(file);
+
+profilePicInput.addEventListener("change", (e) => {
+    getDataUrl(e);
 })
-
-displayUserProfile(profileData);
-
-
-
 
 
